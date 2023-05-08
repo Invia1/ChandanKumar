59069863aa56d53b720a1314fd4e91fab0cbba2d
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-08 13:06:16 +0530

    Add files via upload

---
+++ VarExamples.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class PersonDetails
+    {
+        string pname;
+
+        public void ChangeName(String n)
+        {
+            pname = n;
+            var name = "Chandan"; // var ="string";
+            name = "Bhuwan";
+            Console.WriteLine(name);
+        }
+
+        public static void Main()
+        {
+            var obj = new PersonDetails();
+            obj.pname = "deepa";
+            Console.WriteLine(obj.pname);
+            obj.pname = "Deepak Shrivastava";
+
+            obj.ChangeName("RadheShyam");
+            Console.WriteLine(obj.pname);
+        }
+    }
+}
