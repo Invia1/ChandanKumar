d10b2bbdc152531c9e68d6f820535e6648d2c911
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-08 12:53:52 +0530

    Add files via upload

---
+++ StaticConstructor.cs
@@ -0,0 +1,24 @@
+﻿class StaticConstructor
+{
+    static string officeAddress;
+    static readonly int lwp;
+
+    static StaticConstructor()
+    {
+        officeAddress = "LajpatNagar";
+        lwp = 2;
+    }
+    public static void Main()
+    {
+        Console.WriteLine(officeAddress);
+        
+        Console.WriteLine(lwp);
+
+   //     lwp = 4;
+
+        officeAddress = "noida";
+
+       Console.WriteLine(lwp+" "+officeAddress);
+    }
+
+}
