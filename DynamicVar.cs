8760fbe60a67fe2c9f5e8b0a6fb99583db2c745b
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-08 13:14:13 +0530

    Add files via upload

---
+++ DynamicVar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class PersonDetails2
+    {
+            string pname;
+
+            public void ChangeName(String n)
+            {
+                pname = n;
+                dynamic name = "Chandan"; // var ="string";
+                name = 2.5;
+                Console.WriteLine(name);
+            }
+
+            public static void Main()
+            {
+                var obj = new PersonDetails2();
+                obj.pname = "deepa";
+                Console.WriteLine(obj.pname);
+
+                obj.pname = "Deepak Shrivastava";
+                Console.WriteLine(obj.pname);
+
+
+                obj.ChangeName("RadheShyam");
+                Console.WriteLine(obj.pname);
+            }
+        }
+    }
